01a271235b8135eec2df22a8e33ba48136bde0c3
yarinperetz1313-hash <yarinperetz1313@gmail.com>
2025-08-29 01:34:16 -0500

    Added validated UIController module

--- src/ReplicatedStorage/Modules/UIController.lua
+++ src/ReplicatedStorage/Modules/UIController.lua
@@ -1,18 +1,20 @@
-+--[[
-+    UIController.lua
-+    Centralised collection of BindableEvents used for client <-> client
-+    communication between user interface systems.  Each event is wrapped
-+    in a table for organised access.
-+--]]
-+
-+local UIController = {}
-+
-+UIController.Events = {
-+    ToggleInventory = Instance.new("BindableEvent"),
-+    ToggleShop = Instance.new("BindableEvent"),
-+    ToggleSettings = Instance.new("BindableEvent"),
-+    ToggleMenu = Instance.new("BindableEvent"),
-+}
-+
-+return UIController
-+
+--[[
+    UIController.lua
+    Centralised collection of BindableEvents used for client <-> client
+    communication between user interface systems.  Each event is wrapped
+    in a table for organised access.
+--]]
+
+local UIController = {}
+
+UIController.Events = {
+    ToggleInventory = Instance.new("BindableEvent"),
+    ToggleShop = Instance.new("BindableEvent"),
+    ToggleSettings = Instance.new("BindableEvent"),
+    ToggleMenu = Instance.new("BindableEvent"),
+}
+for name, event in pairs(UIController.Events) do
+    assert(event, ("UIController event missing: %s"):format(name))
+end
+
+return UIController
